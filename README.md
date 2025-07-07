b0027cfccd07a1c9b5e3c79b43008a8b8a3b2b80
dazhangyu123 <1013298661@qq.com>
2025-07-08 07:33:11 +0800

    delete top-k strategy.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This repository contains the PyTorch implementation of our paper "[AEM: Attentio
 ## News
 **[2025.6]** Our paper has been accepted to MICCAI 2025! 🎉 We have updated the arXiv version of our paper with the camera-ready version.
 
-**[2025.4]** We enhanced the AEM by applying it to attention values of the top-k instances (see Lines 228-235 in [main.py](main.py)). Based on our experiments, setting ```k=100``` and ```lambda=0.1``` consistently produced stable results. We welcome you to try these parameters and share your feedback.
+~~**[2025.4]** We enhanced the AEM by applying it to attention values of the top-k instances (see Lines 228-235 in [main.py](main.py)). Based on our experiments, setting ```k=100``` and ```lambda=0.1``` consistently produced stable results. We welcome you to try these parameters and share your feedback.~~
 
 ## Overview
 
--- main.py
+++ main.py
@@ -24,15 +24,13 @@ import torchmetrics
 import time
 import wandb
 import scipy.stats as stats
-import numpy as np
-from typing import Tuple
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 criterion = nn.CrossEntropyLoss()
 
 def get_arguments():
     parser = argparse.ArgumentParser('WSI classification training', add_help=False)
-    parser.add_argument('--config', dest='config', default='config/bracs_config.yml',
+    parser.add_argument('--config', dest='config', default='config/brca_er_config.yml',
                         help='settings of dataset in yaml format')
     parser.add_argument(
         "--seed", type=int, default=2, help="set the random seed to ensure reproducibility"
@@ -43,8 +41,7 @@ def get_arguments():
                         help='the MIL method choice')
     parser.add_argument("--lamda", type=float, default=0.1,
                         help='lambda used for balancing cross-entropy loss and rank loss.')
-    parser.add_argument("--top_k", default=-1, type=int, help="Top-K instances for using the AEM loss.")
-    parser.add_argument('--pretrain', default='conch', choices=['natural_supervised', 'medical_ssl', 'plip', 'path-clip-B-AAAI',
+    parser.add_argument('--pretrain', default='conch', choices=['natural_supervised', 'medical_ssl', 'plip', 'path-clip-B-AAAI', 'titan',
                                                                       'path-clip-B', 'path-clip-L-336', 'openai-clip-B', 'conch',
                                                                      'openai-clip-L-336', 'quilt-net', 'biomedclip', 'UNI', 'GigaPath'],
                         help='pretrain methods')
@@ -79,7 +76,7 @@ def main():
             or conf.pretrain == 'quilt-net'  or conf.pretrain == 'path-clip-B-AAAI'  or conf.pretrain == 'biomedclip' or conf.pretrain == 'conch':
         conf.D_feat = 512
         conf.D_inner = 256
-    elif conf.pretrain == 'path-clip-L-336' or conf.pretrain == 'openai-clip-L-336':
+    elif conf.pretrain == 'path-clip-L-336' or conf.pretrain == 'openai-clip-L-336' or conf.pretrain == 'titan':
         conf.D_feat = 768
         conf.D_inner = 384
     elif conf.pretrain == 'UNI':
@@ -93,14 +90,13 @@ def main():
     # start a new wandb run to track this script
     wandb.init(
         # set the wandb project where this run will be logged
-        project="ADR",
+        project="AEM",
         # track hyperparameters and run metadata
-        config={'dataset': conf.dataset,
+        config={'dataset': conf.config.split('/')[-1].replace("_config.yml", ""),
                 'pretrain': conf.pretrain,
-                'loss_form': conf.arch,
+                'arch': conf.arch,
                 'lamda': conf.lamda,
                 'subsampling': conf.subsampling,
-                'top_k': conf.top_k,
                 'seed': conf.seed,},
         mode=conf.wandb_mode
     )
@@ -120,9 +116,9 @@ def main():
     train_data, val_data, test_data = build_HDF5_feat_dataset(os.path.join(conf.data_dir, 'patch_feats_pretrain_%s.h5'%conf.pretrain), conf)
 
     train_loader = DataLoader(train_data, batch_size=conf.B, shuffle=True,
-                              num_workers=conf.n_worker, pin_memory=conf.pin_memory, drop_last=True, persistent_workers=True)
+                              num_workers=conf.n_worker, pin_memory=conf.pin_memory, drop_last=True, persistent_workers=False)
     val_loader = DataLoader(val_data, batch_size=conf.B, shuffle=False,
-                             num_workers=conf.n_worker, pin_memory=conf.pin_memory, drop_last=False, persistent_workers=True)
+                             num_workers=conf.n_worker, pin_memory=conf.pin_memory, drop_last=False, persistent_workers=False)
     test_loader = DataLoader(test_data, batch_size=conf.B, shuffle=False,
                              num_workers=conf.n_worker, pin_memory=conf.pin_memory, drop_last=False, persistent_workers=True)
 
@@ -219,20 +215,13 @@ def train_one_epoch(model, data_loader, optimizer, device, epoch, conf):
             max_preds, _ = torch.max(ins_preds, 0, keepdim=True)
             bag_loss = 0.5 * criterion(max_preds, labels) + 0.5 * criterion(bag_preds, labels)
         else:
-            bag_logit, attn = model(image_patches, is_train=True)
+            bag_logit, attn = model(image_patches)
             bag_loss = criterion(bag_logit, labels)
 
         if conf.arch == 'mha' or conf.arch == 'dsmil':
             div_loss = torch.sum(F.softmax(attn, dim=-1) * F.log_softmax(attn, dim=-1)) / attn.shape[0]
         else:
-            if conf.top_k > 0:
-                A_softmax = torch.softmax(attn, dim=-1)
-                values, _ = torch.topk(A_softmax, min(conf.top_k, A_softmax.size(-1)), dim=-1)
-                values_sum = values.sum(dim=-1, keepdim=True)
-                values_normalized = values / values_sum
-                div_loss = torch.sum(values_normalized * torch.log(values_normalized))
-            else:
-                div_loss = torch.sum(F.softmax(attn, dim=-1) * F.log_softmax(attn, dim=-1))
+            div_loss = torch.sum(F.softmax(attn, dim=-1) * F.log_softmax(attn, dim=-1))
 
 
 
@@ -274,8 +263,8 @@ def evaluate(model, data_loader, device, conf, header):
 
     y_pred = []
     y_true = []
-    attention_entropies = []
-    validation_losses = []
+    # attention_entropies = []
+    # validation_losses = []
 
     metric_logger = MetricLogger(delimiter="  ")
     for data in metric_logger.log_every(data_loader, 100, header):
@@ -297,8 +286,8 @@ def evaluate(model, data_loader, device, conf, header):
             pred = torch.softmax(bag_logit, dim=-1)
 
         # 记录每个batch的loss和entropy
-        attention_entropies.append(div_loss.item())
-        validation_losses.append(loss.item())
+        # attention_entropies.append(div_loss.item())
+        # validation_losses.append(loss.item())
         acc1 = accuracy(pred, label, topk=(1,))[0]
 
         metric_logger.update(loss=loss.item())
@@ -311,24 +300,24 @@ def evaluate(model, data_loader, device, conf, header):
     y_pred = torch.cat(y_pred, dim=0)
     y_true = torch.cat(y_true, dim=0)
 
-    # 计算Pearson相关系数
-    correlation, p_value = stats.pearsonr(attention_entropies, validation_losses)
+    # # 计算Pearson相关系数
+    # correlation, p_value = stats.pearsonr(attention_entropies, validation_losses)
 
-    AUROC_metric = torchmetrics.AUROC(task="multiclass", num_classes = conf.n_class, average = 'macro').to(device)
-    AUROC_metric(y_pred, y_true)
-    auroc = AUROC_metric.compute().item()
-    F1_metric = torchmetrics.F1Score(task="multiclass", num_classes = conf.n_class, average = 'macro').to(device)
-    F1_metric(y_pred, y_true)
-    f1_score = F1_metric.compute().item()
-    # AUROC_metric = torchmetrics.AUROC(num_classes = conf.n_class, average = 'macro').to(device)
+    # AUROC_metric = torchmetrics.AUROC(task="multiclass", num_classes = conf.n_class, average = 'macro').to(device)
     # AUROC_metric(y_pred, y_true)
     # auroc = AUROC_metric.compute().item()
-    # F1_metric = torchmetrics.F1Score(num_classes = conf.n_class, average = 'macro').to(device)
+    # F1_metric = torchmetrics.F1Score(task="multiclass", num_classes = conf.n_class, average = 'macro').to(device)
     # F1_metric(y_pred, y_true)
     # f1_score = F1_metric.compute().item()
+    AUROC_metric = torchmetrics.AUROC(num_classes = conf.n_class, average = 'macro').to(device)
+    AUROC_metric(y_pred, y_true)
+    auroc = AUROC_metric.compute().item()
+    F1_metric = torchmetrics.F1Score(num_classes = conf.n_class, average = 'macro').to(device)
+    F1_metric(y_pred, y_true)
+    f1_score = F1_metric.compute().item()
 
-    print('* Acc@1 {top1.global_avg:.3f} loss {losses.global_avg:.3f} div_loss {div_losses.global_avg:.3f} auroc {AUROC:.3f} f1_score {F1:.3f} Correlation {correlation: .3f}'
-          .format(top1=metric_logger.acc1, losses=metric_logger.loss, div_losses=metric_logger.div_loss, AUROC=auroc, F1=f1_score, correlation=correlation))
+    print('* Acc@1 {top1.global_avg:.3f} loss {losses.global_avg:.3f} div_loss {div_losses.global_avg:.3f} auroc {AUROC:.3f} f1_score {F1:.3f}'
+          .format(top1=metric_logger.acc1, losses=metric_logger.loss, div_losses=metric_logger.div_loss, AUROC=auroc, F1=f1_score))
 
     if conf.wandb_mode != 'disabled':
         wandb.log({'%s/acc1'%header: metric_logger.acc1.global_avg}, commit=False)
@@ -336,7 +325,7 @@ def evaluate(model, data_loader, device, conf, header):
         wandb.log({'%s/f1'%header: f1_score}, commit=False)
         wandb.log({'%s/loss'%header: metric_logger.loss.global_avg}, commit=False)
         wandb.log({'%s/div_loss'%header: metric_logger.div_loss.global_avg}, commit=False)
-        wandb.log({'%s/pearson_correlation' % header: correlation}, commit=False)
+        # wandb.log({'%s/pearson_correlation' % header: correlation}, commit=False)
 
     return auroc, metric_logger.acc1.global_avg, f1_score, metric_logger.loss.global_avg, metric_logger.div_loss.global_avg
 
